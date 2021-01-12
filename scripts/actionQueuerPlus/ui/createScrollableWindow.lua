67979536005e908370649a8709fc2d9320abf64d
AlexXsWx <psy.suport@gmail.com>
2021-01-12 03:36:47 +0100

    Update options popup to newer looks

--- scripts/actionQueuerPlus/ui/createScrollableWindow.lua
+++ scripts/actionQueuerPlus/ui/createScrollableWindow.lua
@@ -1,135 +1,105 @@
-local Menu           = require "widgets/menu"
+-- local Menu           = require "widgets/menu"
 local Text           = require "widgets/text"
-local Image          = require "widgets/image"
+-- local Image          = require "widgets/image"
 local Widget         = require "widgets/widget"
-local TEMPLATES      = require "widgets/templates"
+local TEMPLATES      = require "widgets/redux/templates"
 
 -- foward declaration --
-local addRoot
-local addBlack
-local addShield
+local addDialog
+local addHeader
 local addTitle
-local addMenu
 ------------------------
 
 local createScrollableWindow = function(params)
-    -- addBlack({ parent = params.parent })
 
-    local root = addRoot({ parent = params.parent })
+    params.parent:AddChild(TEMPLATES.BackgroundTint())
 
-    addShield({ parent = root })
+    local root = params.parent:AddChild(TEMPLATES.ScreenRoot())
+
+    local dialog = addDialog({
+        parent    = root,
+        itemWidth = params.itemWidth,
+        onApply   = params.onApply,
+        onCancel  = params.onCancel,
+    })
+
+    local header = addHeader({ parent = dialog })
 
     addTitle({
-        parent = root,
+        parent = header,
         title = params.title
     })
 
-    addMenu({
-        parent   = root,
-        onApply  = params.onApply,
-        onCancel = params.onCancel,
-    })
-
     local window = {
-        root = root
+        root = root,
+        dialog = dialog,
+        header = header,
     }
 
     return window
 end
 
-addRoot = function(params)
-    local parent = params.parent
+addDialog = function(params)
+    local parent    = params.parent
+    local itemWidth = params.itemWidth
+    local onApply   = params.onApply
+    local onCancel  = params.onCancel
 
-    local root = parent:AddChild(Widget("ROOT"))
-    root:SetVAnchor(ANCHOR_MIDDLE)
-    root:SetHAnchor(ANCHOR_MIDDLE)
-    root:SetPosition(0, 0, 0)
-    root:SetScaleMode(SCALEMODE_PROPORTIONAL)
+    local buttons = {
+        { text = STRINGS.UI.MODSSCREEN.APPLY, cb = onApply  },
+        { text = STRINGS.UI.MODSSCREEN.BACK,  cb = onCancel },
+    }
 
-    return root
+    local dialog = parent:AddChild(
+        TEMPLATES.RectangleWindow(itemWidth + 20, 580, nil, buttons)
+    )
+
+    return dialog
 end
 
-addBlack = function(params)
+addHeader = function(params)
     local parent = params.parent
 
-    local black = parent:AddChild(
-        Image("images/global.xml", "square.tex")
-    )
-    black:SetVRegPoint(ANCHOR_MIDDLE)
-    black:SetHRegPoint(ANCHOR_MIDDLE)
-    black:SetVAnchor(ANCHOR_MIDDLE)
-    black:SetHAnchor(ANCHOR_MIDDLE)
-    black:SetScaleMode(SCALEMODE_FILLSCREEN)
-    black:SetTint(0, 0, 0, 0.75)
+    local header = parent:AddChild(Widget("option_header"))
+    header:SetPosition(0, 270)
 
-    return black
-end
-
-addShield = function(params)
-    local parent = params.parent
-
-    local shield = parent:AddChild(TEMPLATES.CurlyWindow(40, 365, 1, 1, 67, -41))
-    local fill = parent:AddChild(
-        Image("images/fepanel_fills.xml", "panel_fill_tall.tex")
-    )
-    fill:SetScale(0.64, -0.57)
-    fill:SetPosition(8, 12)
-    shield:SetPosition(0, 0, 0)
-
-    return shield, fill
+    return header
 end
 
 addTitle = function(params)
     local parent   = params.parent
     local titleStr = params.title
 
+    local suffix = STRINGS.UI.MODSSCREEN.CONFIGSCREENTITLESUFFIX
+
     local title_max_w = 420
     local title_max_chars = 70
     local title = parent:AddChild(
-        Text(BUTTONFONT, 45, " "..STRINGS.UI.MODSSCREEN.CONFIGSCREENTITLESUFFIX)
+        Text(HEADERFONT, 28, " "..suffix)
     )
     local title_suffix_w = title:GetRegionSize()
-    title:SetPosition(10, 190)
-    title:SetColour(0, 0, 0, 1)
+    title:SetColour(UICOLOURS.GOLD_SELECTED)
     if title_suffix_w < title_max_w then
         title:SetTruncatedString(
             titleStr,
             title_max_w - title_suffix_w,
-            title_max_chars - 1 - STRINGS.UI.MODSSCREEN.CONFIGSCREENTITLESUFFIX:len(),
+            title_max_chars - 1 - suffix:len(),
             true
         )
-        title:SetString(title:GetString().." "..STRINGS.UI.MODSSCREEN.CONFIGSCREENTITLESUFFIX)
+        title:SetString(
+            title:GetString().." "..suffix
+        )
     else
+        -- translation was so long we can't fit any more text
         title:SetTruncatedString(
-            STRINGS.UI.MODSSCREEN.CONFIGSCREENTITLESUFFIX, title_max_w, title_max_chars, true
+            suffix,
+            title_max_w,
+            title_max_chars,
+            true
         )
     end
 
     return title
-end
-
-addMenu = function(params)
-    local parent   = params.parent
-    local onApply  = params.onApply
-    local onCancel = params.onCancel
-
-    local menu = parent:AddChild(Menu(nil, 0, true))
-    local applybutton = menu:AddItem(
-        STRINGS.UI.MODSSCREEN.APPLY,
-        onApply,
-        Vector3(165, -230, 0),
-        "large"
-    )
-    local cancelbutton = menu:AddItem(
-        STRINGS.UI.MODSSCREEN.BACK,
-        onCancel,
-        Vector3(-155, -230, 0)
-    )
-    applybutton:SetScale(0.7)
-    cancelbutton:SetScale(0.7)
-    menu:SetPosition(5, 0)
-
-    return menu
 end
 
 return createScrollableWindow
--- scripts/screens/actionqueuerplusoptionsscreen.lua
+++ scripts/screens/actionqueuerplusoptionsscreen.lua
@@ -1,8 +1,10 @@
 local Screen         = require "widgets/screen"
+local Image = require "widgets/image"
 local Text           = require "widgets/text"
-local Spinner        = require "widgets/spinner"
+-- local Spinner        = require "widgets/spinner"
 local Widget         = require "widgets/widget"
-local ScrollableList = require "widgets/scrollablelist"
+local TEMPLATES = require "widgets/redux/templates"
+-- local ScrollableList = require "widgets/scrollablelist"
 
 local createScrollableWindow = require "actionQueuerPlus/ui/createScrollableWindow"
 
@@ -10,6 +12,7 @@ local createScrollableWindow = require "actionQueuerPlus/ui/createScrollableWind
 local OptionsScreen_Apply
 local getModOptions
 local collectSettings
+local createDescription
 local createOptionsScrollList
 local createOptionWidgets
 -------------------------
@@ -29,19 +32,41 @@ local OptionsScreen = Class(Screen, function(self, modname, scrollViewOffset, on
         self._dirty = true
     end
 
+    local uiParams = {}
+    uiParams.labelWidth = 300
+    uiParams.spinnerWidth = 225
+    uiParams.itemWidth = uiParams.labelWidth + uiParams.spinnerWidth + 30
+    uiParams.itemHeight = 40
+
     local window = createScrollableWindow({
         parent = self,
-        title = "Action Queuer Plus Options",
+        title = (
+            KnownModIndex:GetModFancyName(modname)
+            -- FIXME
+            -- .. " " ..
+            -- STRINGS.UI.MODSSCREEN.CONFIGSCREENTITLESUFFIX
+        ),
+        itemWidth = uiParams.itemWidth,
         onApply  = function() OptionsScreen_Apply(self) end,
         onCancel = function() self:Close() end,
     })
 
+    local description = createDescription(window.header, uiParams)
+
     self._optionsScrollList = createOptionsScrollList(
-        window.root,
+        window.dialog,
         self._options,
+        description,
+        uiParams,
         scrollViewOffset,
         markDirty
     )
+
+    if TheInput:ControllerAttached() then
+        window.dialog.actions:Hide()
+    end
+
+    self.default_focus = self._optionsScrollList
 end)
 
 function OptionsScreen:OnControl(control, down)
@@ -61,7 +86,9 @@ end
 
 function OptionsScreen:Close(optHadEffect)
     self._onClose(
-        self._optionsScrollList.view_offset,
+        -- self._optionsScrollList.view_offset,
+        -- FIXME
+        self._optionsScrollList.current_scroll_pos,
         optHadEffect or false
     )
     TheFrontEnd:PopScreen()
@@ -133,110 +160,276 @@ end
 
 -- UI stuff
 
-createOptionsScrollList = function(parent, options, scrollViewOffset, markDirty)
-    local optionsPanel = parent:AddChild(Widget("optionspanel"))  
-    optionsPanel:SetPosition(0, -20)
+createDescription = function(parent, uiParams)
 
-    local scrollList = optionsPanel:AddChild(
-        ScrollableList({}, 450, 350, 40, 10)
-    )
+    local optionDescription = parent:AddChild(Text(CHATFONT, 22))
+    optionDescription:SetColour(UICOLOURS.GOLD_UNIMPORTANT)
+    optionDescription:SetPosition(0, -48)
+    optionDescription:SetRegionSize(uiParams.itemWidth + 30, 50)
+    -- stop text from jumping around as we scroll
+    optionDescription:SetVAlign(ANCHOR_TOP)
+    optionDescription:EnableWordWrap(true)
 
-    local optionWidgets = createOptionWidgets(options, markDirty)
+    local valueDescription = parent:AddChild(Text(CHATFONT, 22))
+    valueDescription:SetColour(UICOLOURS.GOLD)
+    valueDescription:SetPosition(0, -85)
+    valueDescription:SetRegionSize(uiParams.itemWidth + 30, 25)
 
-    scrollList:SetList(optionWidgets)
-    if scrollList.scroll_bar_line:IsVisible() then
-        scrollList:SetPosition(0, 0)
-    else
-        scrollList:SetPosition(-20, 0)
+    return {
+        optionDescription = optionDescription,
+        valueDescription  = valueDescription,
+    }
+
+end
+
+createOptionsScrollList = function(parent, options, description, uiParams, scrollViewOffset, markDirty)
+    local optionsPanel = parent:InsertWidget(Widget("optionspanel"))
+    optionsPanel:SetPosition(0, -60)
+
+    local scrollList = nil
+
+    local optionWidgets = createOptionWidgets(options)
+
+    local function ScrollWidgetsCtor(context, idx)
+        local widget = Widget("option"..idx)
+        widget.bg = widget:AddChild(
+            TEMPLATES.ListItemBackground(uiParams.itemWidth, uiParams.itemHeight)
+        )
+        widget.opt = widget:AddChild(
+            TEMPLATES.LabelSpinner(
+                "", {}, uiParams.labelWidth, uiParams.spinnerWidth, uiParams.itemHeight
+            )
+        )
+
+        widget.opt.spinner:EnablePendingModificationBackground()
+
+        widget.ApplyDescription = function(_)
+            local option = widget.opt.data and widget.opt.data.option.hover or ""
+            local value = (
+                widget.opt.data and
+                widget.opt.data.spin_options_hover[widget.opt.data.selected_value] or
+                ""
+            )
+            description.optionDescription:SetString(option)
+            description.valueDescription:SetString(value)
+        end
+        
+        widget:SetOnGainFocus(function(_)
+            scrollList:OnWidgetFocus(widget)
+            widget:ApplyDescription()
+        end)
+
+        widget.real_index = idx
+        widget.opt.spinner.OnChanged =
+            function( _, data )
+                options[widget.real_index].value = data
+                widget.opt.data.selected_value = data
+                widget.opt.spinner:SetHasModification(
+                    widget.opt.data.selected_value ~= widget.opt.data.initial_value
+                )
+                widget:ApplyDescription()
+                markDirty()
+            end
+
+        widget.focus_forward = widget.opt
+        
+        return widget
     end
 
-    scrollList:Scroll(scrollViewOffset, true)
+    local function ApplyDataToWidget(context, widget, data, idx)
+        widget.opt.data = data
+        if data then
+            widget.real_index = idx
+
+            widget.opt:Show()
+            widget.opt.spinner:SetOptions(data.spin_options)
+
+            if data.is_header then
+                widget.bg:Hide()
+                widget.opt.spinner:Hide()
+                widget.opt.label:SetSize(30)
+            else
+                widget.bg:Show()
+                widget.opt.spinner:Show()
+                widget.opt.label:SetSize(25) -- same as LabelSpinner's default.
+            end
+            
+            widget.opt.spinner:SetSelected(data.selected_value)
+
+            local label = (
+                data.option.label or
+                data.option.name or
+                STRINGS.UI.MODSSCREEN.UNKNOWN_MOD_CONFIG_SETTING
+            )
+            if not data.is_header then
+                label =  label .. ":"
+            end
+            widget.opt.label:SetString(label)
+
+            widget.opt.spinner:SetHasModification(
+                widget.opt.data.selected_value ~= widget.opt.data.initial_value
+            )
+
+            if widget.focus then
+                widget:ApplyDescription()
+            end
+        else
+            widget.opt:Hide()
+            widget.bg:Hide()
+        end
+    end
+
+    scrollList = optionsPanel:AddChild(TEMPLATES.ScrollingGrid(
+        optionWidgets,
+        {
+            scroll_context = {
+            },
+            widget_width  = uiParams.itemWidth,
+            widget_height = uiParams.itemHeight,
+            num_visible_rows = 11,
+            num_columns = 1,
+            item_ctor_fn = ScrollWidgetsCtor,
+            apply_fn = ApplyDataToWidget,
+            scrollbar_offset = 20,
+            scrollbar_height_offset = -60
+        }
+    ))
+    scrollList:SetPosition(0, -6)
+
+    -- Top border of the scroll list.
+    local horizontalLine = optionsPanel:AddChild(
+        Image("images/global_redux.xml", "item_divider.tex")
+    )
+    horizontalLine:SetPosition(0, scrollList.visible_rows / 2 * uiParams.itemHeight)
+    horizontalLine:SetSize(uiParams.itemWidth + 30, 5)
+
+    -- local scrollList = optionsPanel:AddChild(
+    --     ScrollableList({}, 450, 350, 40, 10)
+    -- )
+
+    -- scrollList:SetList(optionWidgets)
+    -- if scrollList.scroll_bar_line:IsVisible() then
+    --     scrollList:SetPosition(0, 0)
+    -- else
+    --     scrollList:SetPosition(-20, 0)
+    -- end
+
+    -- scrollList:Scroll(scrollViewOffset, true)
+
+
+    -- Skip animation
+    -- scrollList.current_scroll_pos = scrollViewOffset
+    -- scrollList:Scroll(scrollViewOffset)
+    scrollList:ScrollToDataIndex(scrollViewOffset)
 
     return scrollList
 end
 
-createOptionWidgets = function(options, markDirty)
+createOptionWidgets = function(options)
     local optionWidgets = {}
 
-    local i = 1
-    local labelWidth = 225
+    -- local i = 1
+    -- local labelWidth = 225
 
-    while i <= #options do
-        if options[i] then
-            local spinOptions = {}
-            local spinOptionsHover = {}
-            local idx = i
-            for _, v in ipairs(options[idx].options) do
-                table.insert(spinOptions, { text = v.description, data = v.data })
-                spinOptionsHover[v.data] = v.hover
-            end
+    -- while i <= #options do
+    --     if options[i] then
+    --         local spinOptions = {}
+    --         local spinOptionsHover = {}
+    --         local idx = i
+    --         for _, v in ipairs(options[idx].options) do
+    --             table.insert(spinOptions, { text = v.description, data = v.data })
+    --             spinOptionsHover[v.data] = v.hover
+    --         end
             
-            local opt = Widget("option"..idx)
+    --         local opt = Widget("option"..idx)
 
-            -- spinner
+    --         -- spinner
             
-            local spinnerWidth = 170
-            local spinnerHeight = 40
-            opt.spinner = opt:AddChild(
-                Spinner(
-                    spinOptions,
-                    spinnerWidth,
-                    nil,
-                    { font = NEWFONT, size = 25 },
-                    nil,
-                    nil,
-                    nil,
-                    true,
-                    100,
-                    nil
-                )
-            )
-            opt.spinner:SetTextColour(0, 0, 0, 1)
-            local defaultValue = options[idx].value
-            if defaultValue == nil then defaultValue = options[idx].default end
+    --         local spinnerWidth = 170
+    --         local spinnerHeight = 40
+    --         opt.spinner = opt:AddChild(
+    --             Spinner(
+    --                 spinOptions,
+    --                 spinnerWidth,
+    --                 nil,
+    --                 { font = NEWFONT, size = 25 },
+    --                 nil,
+    --                 nil,
+    --                 nil,
+    --                 true,
+    --                 100,
+    --                 nil
+    --             )
+    --         )
+    --         opt.spinner:SetTextColour(0, 0, 0, 1)
+    --         local defaultValue = options[idx].value
+    --         if defaultValue == nil then defaultValue = options[idx].default end
             
-            opt.spinner.OnChanged = function(_, data)
-                options[idx].value = data
-                opt.spinner:SetHoverText(spinOptionsHover[data] or "")
-                markDirty()
-            end
-            opt.spinner:SetSelected(defaultValue)
-            opt.spinner:SetHoverText(spinOptionsHover[defaultValue] or "")
-            opt.spinner:SetPosition(325, 0, 0)
+    --         opt.spinner.OnChanged = function(_, data)
+    --             options[idx].value = data
+    --             opt.spinner:SetHoverText(spinOptionsHover[data] or "")
+    --             markDirty()
+    --         end
+    --         opt.spinner:SetSelected(defaultValue)
+    --         opt.spinner:SetHoverText(spinOptionsHover[defaultValue] or "")
+    --         opt.spinner:SetPosition(325, 0, 0)
 
-            -- label
+    --         -- label
 
-            local label = opt.spinner:AddChild(
-                Text(
-                    NEWFONT,
-                    25,
-                    (
-                        options[idx].label or
-                        options[idx].name or
-                        STRINGS.UI.MODSSCREEN.UNKNOWN_MOD_CONFIG_SETTING
-                    ) .. ":"
-                )
-            )
-            label:SetColour(0, 0, 0, 1)
-            label:SetPosition(-labelWidth / 2 - 90, 0, 0)
-            label:SetRegionSize(labelWidth, 50)
-            label:SetHAlign(ANCHOR_RIGHT)
-            label:SetHoverText(options[idx].hover or "")
-            if TheInput:ControllerAttached() then
-                opt:SetHoverText(options[idx].hover or "")
-            end
+    --         local label = opt.spinner:AddChild(
+    --             Text(
+    --                 NEWFONT,
+    --                 25,
+    --                 (
+    --                     options[idx].label or
+    --                     options[idx].name or
+    --                     STRINGS.UI.MODSSCREEN.UNKNOWN_MOD_CONFIG_SETTING
+    --                 ) .. ":"
+    --             )
+    --         )
+    --         label:SetColour(0, 0, 0, 1)
+    --         label:SetPosition(-labelWidth / 2 - 90, 0, 0)
+    --         label:SetRegionSize(labelWidth, 50)
+    --         label:SetHAlign(ANCHOR_RIGHT)
+    --         label:SetHoverText(options[idx].hover or "")
+    --         if TheInput:ControllerAttached() then
+    --             opt:SetHoverText(options[idx].hover or "")
+    --         end
 
-            opt.spinner.OnGainFocus = function(self)
-                Spinner._base.OnGainFocus(self)
-                opt.spinner:UpdateBG()
-            end
-            opt.focus_forward = opt.spinner
+    --         opt.spinner.OnGainFocus = function(self)
+    --             Spinner._base.OnGainFocus(self)
+    --             opt.spinner:UpdateBG()
+    --         end
+    --         opt.focus_forward = opt.spinner
 
-            opt.id = idx
+    --         opt.id = idx
             
-            table.insert(optionWidgets, opt)
+    --         table.insert(optionWidgets, opt)
+    --     end
+    --     i = i + 1
+    -- end
+
+    for idx, option_item in ipairs(options) do
+        local spin_options = {}
+        local spin_options_hover = {}
+        for _,v in ipairs(option_item.options) do
+            table.insert(spin_options, { text = v.description, data = v.data })
+            spin_options_hover[v.data] = v.hover
         end
-        i = i + 1
+        local initial_value = option_item.value
+        if initial_value == nil then
+            initial_value = option_item.default
+        end
+        local data = {
+            is_header = #spin_options == 1 and spin_options[1].text:len() == 0,
+            option = option_item,
+            initial_value = initial_value,
+            selected_value = initial_value,
+            spin_options = spin_options,
+            spin_options_hover = spin_options_hover,
+        }
+
+        table.insert(optionWidgets, data)
     end
 
     return optionWidgets
