7c54fac767094cd3999e01011725a7bc37be52f5
AlexXsWx <psy.suport@gmail.com>
2021-01-20 03:17:21 +0100

    Bump version to 1.2.0

--- modinfo.lua
+++ modinfo.lua
@@ -1,6 +1,6 @@
 name = "Autopilot"
 author = "AlexXsWx"
-version = "1.1.0"
+version = "1.2.0"
 
 description = (
     "This mod is a rewrite and extending of ActionQueue(DST) v1.3.6 by simplex and then xiaoXzzz\n"..
